b6508d96c4964446afe61ecd5824ab5c2e116803
Cedric H <159480174+Cedric-Hj@users.noreply.github.com>
2024-11-20 18:29:45 +0000

    Update tests.py

--- Test/Playwright/tests.py
+++ Test/Playwright/tests.py
@@ -17,18 +17,18 @@ def browser_context(browser):
 
 def test_page_title(browser_context):
     with browser_context.new_page() as page:
-        page.goto('http://192.168.0.101:31804/', timeout=5000)
+        page.goto('http://192.168.0.101:31806/', timeout=5000)
         title = page.title()
         assert title == "Ced's Webpage"  # Verify the title matches the one in the HTML
 
 def test_dev_environment_text(browser_context):
     with browser_context.new_page() as page:
-        page.goto('http://192.168.0.101:31804/', timeout=5000)
+        page.goto('http://192.168.0.101:31806/', timeout=5000)
         dev_text_visible = page.locator("text=dev environment").is_visible(timeout=5000)
         assert dev_text_visible is True  # Ensure the "dev environment" text is visible
 
 def test_version_text(browser_context):
     with browser_context.new_page() as page:
-        page.goto('http://192.168.0.101:31804/', timeout=5000)
+        page.goto('http://192.168.0.101:31806/', timeout=5000)
         version_text = page.locator("#version").text_content(timeout=5000)
         assert version_text == 'v2.0.3'  # Ensure the version is displayed correctly
