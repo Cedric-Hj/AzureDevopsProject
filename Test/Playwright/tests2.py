d6394f132cbf6e4029be870f1260332c56d96105
Cedric H <159480174+Cedric-Hj@users.noreply.github.com>
2024-11-20 19:34:18 +0000

    Update tests2.py

--- Test/Playwright/tests2.py
+++ Test/Playwright/tests2.py
@@ -10,24 +10,36 @@ def browser():
 
 @pytest.fixture(scope="function")
 def browser_context(browser):
-    context = browser.new_context(record_video_dir="/var/lib/jenkins/myagent/_work/6/s/playwright-report/videos/")  # Specify the directory to save videos
+    context = browser.new_context()
     yield context
     context.close()
 
 def test_page_title(browser_context):
     with browser_context.new_page() as page:
-        page.goto('http://192.168.0.101:31806/', timeout=5000)
-        title = page.title()
-        assert title == "Ced's Webpage"  # Verify the title matches the one in the HTML
+        try:
+            page.goto('http://192.168.0.101:31806/', timeout=5000)
+            title = page.title()
+            assert title == "Ced's Webpage"  # Verify the title matches the one in the HTML
+        except AssertionError:
+            page.screenshot(path="playwright-report/test_page_title_failure.png")
+            raise
 
 def test_dev_environment_text(browser_context):
     with browser_context.new_page() as page:
-        page.goto('http://192.168.0.101:31806/', timeout=5000)
-        dev_text_visible = page.locator("text=dev environment").is_visible(timeout=5000)
-        assert dev_text_visible is True  # Ensure the "dev environment" text is visible
+        try:
+            page.goto('http://192.168.0.101:31806/', timeout=5000)
+            dev_text_visible = page.locator("text=dev environment").is_visible(timeout=5000)
+            assert dev_text_visible is True  # Ensure the "dev environment" text is visible
+        except AssertionError:
+            page.screenshot(path="playwright-report/test_dev_environment_text_failure.png")
+            raise
 
 def test_version_text(browser_context):
     with browser_context.new_page() as page:
-        page.goto('http://192.168.0.101:31806/', timeout=5000)
-        version_text = page.locator("#version").text_content(timeout=5000)
-        assert version_text == 'v2.0.3'  # Ensure the version is displayed correctly
+        try:
+            page.goto('http://192.168.0.101:31806/', timeout=5000)
+            version_text = page.locator("#version").text_content(timeout=5000)
+            assert version_text == 'v2.0.3'  # Ensure the version is displayed correctly
+        except AssertionError:
+            page.screenshot(path="playwright-report/test_version_text_failure.png")
+            raise
