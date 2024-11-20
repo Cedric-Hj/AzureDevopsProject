674cc6132ca022c5b5160a77948212f53aa85dba
Cedric H <159480174+Cedric-Hj@users.noreply.github.com>
2024-11-20 19:10:02 +0000

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -1,5 +1,11 @@
 import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
-  reporter: [['html', { open: 'never' }]],
+  reporter: [
+    ['html', { 
+      outputFile: 'playwright-report/index.html', // Save the report to a file
+      open: 'never'  // Disable the report server from starting
+    }]
+  ],
+  // Optional: Set the timeout for test execution or other configurations as needed
 });
