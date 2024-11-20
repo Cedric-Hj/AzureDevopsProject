b98bc56e11e1ec85bfd59b5379dbdc9b9aac4f7d
Cedric H <159480174+Cedric-Hj@users.noreply.github.com>
2024-11-20 19:03:46 +0000

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -1,9 +1,5 @@
 import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
-  reporter: [
-    ['json', { outputFile: 'playwright-report/results.json' }],  // Save results in JSON format
-    ['html', { outputFile: 'playwright-report/index.html', open: 'never' }]  // Save HTML but prevent server
-  ],
-  // Optional: Set the timeout for test execution or other configurations as needed
+  reporter: [['html', { open: 'never' }]],
 });
